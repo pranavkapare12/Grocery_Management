e86e8ecd80fd37bc51656847a7f0268cdf756445
Pranav Kapare <sarangkapare12345@gmail.com>
2026-05-18 12:58:06 +0530

    Create Products Schema

--- backend/Database/ProductSchema.js
+++ backend/Database/ProductSchema.js
@@ -1,4 +1,26 @@
 import mongoose, { Schema } from "mongoose"
 const Schema = new Schema({
-    
-})
+    public_id:{
+        type:String,
+        require:true
+    },
+    url:{
+        type:String,
+        require:true
+    },
+    product_name:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        require:true
+    },
+    user_id:{
+        type:String,
+        require:true
+    }
+},{timestamp:true})
+
+const Product = mongoose.model("Product",Schema,"Products");
+export default Product;
