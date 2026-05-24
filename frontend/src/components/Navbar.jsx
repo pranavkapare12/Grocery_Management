67c6a8cb1a2496bdbbf2a55ef59921f8ac6430c8
Pranav Kapare <sarangkapare12345@gmail.com>
2026-05-24 22:23:59 +0530

    Add logout button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,35 @@
 import react from 'react';
+import { AuthContext } from '../context/AuthProvider';
 import { Link } from 'react-router-dom';
 import { CircleUser } from 'lucide-react';
+import { useContext , useState } from 'react';
+import axios from 'axios';
+
 function Navbar() {
+    const { user,setUser } = useContext(AuthContext);
+    const [ loding , setLoading ] = useState(false);
+    async function logout(){
+        try {
+            setLoading(true)
+            const conform = window.confirm("Do you Want to logout")
+            if(conform){
+                let result = await axios.post("http://localhost:3000/auth/logout", { withCredintial:true }).then((data)=>{
+                    console.log(data);
+                    setUser(null)
+                })
+            }else{
+                return;
+                setLoading(false)
+            }
+
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    console.log(user)
     return (
         <div className=' relative top-0 w-full h-15 flex justify-between place-items-center px-1 sm:px-1 md:px-3 lg:px-12 shadow-lg' style={{ fontFamily: "'Inria Sans', sans-serif" }}>
             <div className=" font-bold text-2xl flex place-items-center" >
@@ -26,6 +54,9 @@ function Navbar() {
                     <img src="/shopping-cart.png" className=' size-8' />
                     <span className=' relative border-0 text-sm'>0</span>
                 </Link>
+                <button className='bg-blue-500 px-2 py-1 rounded-2xl text-white font-mono border-0 active:scale-90' onClick={logout}>
+                    Logout
+                </button>
             </div>
         </div>
     )
