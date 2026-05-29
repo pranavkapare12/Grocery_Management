f3807367417365c8443dc6e77814f1eb73e9feb0
Pranav Kapare <sarangkapare12345@gmail.com>
2026-05-29 18:42:58 +0530

    fix bugs

--- backend/Controler/Data.controler.js
+++ backend/Controler/Data.controler.js
@@ -1,29 +1,68 @@
 import mongoDb from "../Database/DbConnection.js";
 import Product from "../Database/ProductSchema.js";
 
-async function getData(req,res){
+async function getData(req, res) {
     let conn = mongoDb();
 
-    if (req.userData.type === "Customer"){
+    if (req.userData.type === "Customer") {
         let result = await Product.find({});
         return res.status(200).json({
             userData: req.userData,
-            data : result
+            data: result
         })
     }
 
-    if (req.userData.type === "Vendor"){
+    if (req.userData.type === "Vendor") {
         let result = await Product.find({
             user_id: req.userData._id
         });
         return res.status(200).json({
             userData: req.userData,
-            products : result
+            products: result
         })
     }
-    
+
     res.status(200).json({
-        "message":"All is working correctly"
+        "message": "All is working correctly"
     })
 }
-export { getData };
+
+async function updateData(req, res) {
+    let conn = mongoDb();
+
+    if (req.userData.type === "Vendor") {
+        let result = await Product.updateOne({
+            _id: req.body._id
+        },{
+            $set:{
+                product_name: req.body.product_name,
+                description: req.body.description,
+                price: req.body.price,
+                stock: req.body.stock,
+                category: req.body.category,
+                brand: req.body.brand,
+                unit: req.body.unit
+            }
+        })
+        if(result.acknowledged ){
+            return res.status(200).json({
+                "message": "Data Updated Successfully"
+            })
+        }else{
+            return res.status(500).json({
+                "message": "Failed to update data"
+            })
+        }
+    }
+
+    if (req.userData.type === "Customer") {
+        res.status(401).json({
+            "message": "Customers are not allowed to update data"
+        })
+    }
+
+    res.status(200).json({
+        "message": "All is working correctly"
+    })
+}
+export { getData, updateData };
--- backend/Routes/Data.routes.js
+++ backend/Routes/Data.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/Auth.middleware.js";
-import { getData } from "../Controler/Data.controler.js";
+import { getData , updateData } from "../Controler/Data.controler.js";
 const datarouter = Router();
 
 datarouter.get("/",authMiddleware,getData);
+datarouter.put("/update",authMiddleware,updateData);
 
 export default datarouter;
