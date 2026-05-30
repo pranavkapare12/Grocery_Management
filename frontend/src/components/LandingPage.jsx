bf912c85b9f925e518b4997ae2df2513c4de4293
Pranav Kapare <sarangkapare12345@gmail.com>
2026-05-30 16:12:08 +0530

    upgrade function

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -6,9 +6,15 @@ import { useState } from "react";
 
 function Landing() {
 
-    const [state,setState]=useState({
-        fir_pass:false,
-        sec_pass:false
+    const [state, setState] = useState({
+        fir_pass: false,
+        sec_pass: false
+    })
+
+    const [userData, setUserDate] = useState({
+        email: "",
+        fir_pass: "",
+        sec_pass: ""
     })
 
 
@@ -37,36 +43,43 @@ function Landing() {
                             className="flex-1"
                         />
                     </div>
-                    <div className="flex justify-center w-full gap-x-4">
-                        <input 
-                        type={state.fir_pass ? "text" : "password"} 
-                        placeholder="Password" className=" bg-white border-2 px-4 py-2" />
-                        <button className=" border-2 px-2 py-2 rounded-[5px]"
-                        onClick={()=> setState({...state,fir_pass: !state.fir_pass})}
-                        >
-                            {
-                                state.fir_pass ? <EyeClosed size={20} /> : <Eye size={20} />
-                            }
-                        </button>
-                    </div>
-                    <div className="flex justify-center w-full gap-x-4">
-                        <input 
-                        type={state.sec_pass ? "text" : "password"} 
-                        placeholder="Conform Password" 
-                        className=" bg-white border-2 px-4 py-2" />
-                        <button className=" border-2 px-2 py-2 rounded-[5px]"
-                        onClick={()=> setState({...state,sec_pass: !state.sec_pass})}
-                        >
-                            {
-                                state.sec_pass ? <EyeClosed size={20} /> : <Eye size={20} />
-                            }
-                        </button>
-                    </div>
-                    <div className="flex justify-center w-full gap-x-4">
-                        <button className=" border-2 px-5 py-2 bg-black rounded-xl">
-                            <label htmlFor="" className=" text-white font-semibold">Continue</label>
-                        </button>
-                    </div>
+
+                    {
+                        userData.email === "" ? "" :
+                            <>
+                                <div className="flex justify-center w-full gap-x-4">
+                                    <input
+                                        type={state.fir_pass ? "text" : "password"}
+                                        placeholder="Password" className=" bg-white border-2 px-4 py-2" />
+                                    <button className=" border-2 px-2 py-2 rounded-[5px]"
+                                        onClick={() => setState({ ...state, fir_pass: !state.fir_pass })}
+                                    >
+                                        {
+                                            state.fir_pass ? <EyeClosed size={20} /> : <Eye size={20} />
+                                        }
+                                    </button>
+                                </div>
+                                <div className="flex justify-center w-full gap-x-4">
+                                    <input
+                                        type={state.sec_pass ? "text" : "password"}
+                                        placeholder="Conform Password"
+                                        className=" bg-white border-2 px-4 py-2" />
+                                    <button className=" border-2 px-2 py-2 rounded-[5px]"
+                                        onClick={() => setState({ ...state, sec_pass: !state.sec_pass })}
+                                    >
+                                        {
+                                            state.sec_pass ? <EyeClosed size={20} /> : <Eye size={20} />
+                                        }
+                                    </button>
+                                </div>
+                                <div className="flex justify-center w-full gap-x-4">
+                                    <button className=" border-2 px-5 py-2 bg-black rounded-xl">
+                                        <label htmlFor="" className=" text-white font-semibold">Continue</label>
+                                    </button>
+                                </div>
+                            </>
+                    }
+
                 </div>
             </div>
         </>
