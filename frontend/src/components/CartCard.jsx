065b8cbec40fe8d78d32549c4b0c42c594d4d700
Pranav Kapare <sarangkapare12345@gmail.com>
2026-06-03 11:18:20 +0530

    fix bugs

--- frontend/src/components/CartCard.jsx
+++ frontend/src/components/CartCard.jsx
@@ -1,8 +1,40 @@
 import React from "react";
 import { X, IndianRupee } from 'lucide-react'
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
 
 function CartCard(props) {
-    let data = props.data;
+    const [data, setData ]= useState(props.data);
+    const { cart , addToCart } = useContext(AuthContext);
+    function findId(){
+        let index = cart.findIndex(index => index._id === data._id)
+        return index;
+    }
+
+    function insertIntoCart(){
+        if(data.quantity < 1){
+            toast("Product Quanity Should be greater than 0");
+            return;
+        }
+
+        let index = findId()
+        if(index != -1){
+            const updateCart = cart.map(item =>
+                item._id === data._id ? { ...item , quantity : item.quantity = data.quantity}:
+                item
+            )
+            addToCart(updateCart)
+        }else{
+            addToCart([...cart,data])
+        }
+    }
+
+    const updateQuantity = (e) =>{
+        if(e.target.value > -1){
+            setData(prev => ({...prev,quantity:Number(e.target.value)}))
+            return;
+        }
+    }
     return (
         <div className=" bg-[#ffffff] w-full h-12/12 sm:w-3xl sm:h-9/12 md:w-7/12 lg:w-2/12 flex flex-col border rounded-xs" >
             <div className="flex-2 flex flex-col">
@@ -20,9 +52,11 @@ function CartCard(props) {
             </div>
             <div className="flex-1 flex flex-row row gap-x-4 px-5 py-4 bg-white rounded-b-xl">
                 <div className="flex-1 border flex justify-center items-center rounded-xs">
-                    <label htmlFor="" className="">{data.quantity}</label>
+                    <input type="number" value={data.quantity}  onChange={(e) => updateQuantity(e)}/>
                 </div>
-                <button className=" flex-1 bg-[#FFD558] text-white active:scale-95 rounded-[3px]">Update</button>
+                <button className=" flex-1 bg-[#FFD558] text-white active:scale-95 rounded-[3px]"
+                onClick={insertIntoCart}
+                >Update</button>
             </div>
             <div className="flex-1 bg-white rounded-b-xl flex justify-center items-center">
                 <div className=" flex gap-x-2">
