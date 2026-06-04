dcba7847c75c4b93a6476003d328a69a53581dfc
Pranav Kapare <sarangkapare12345@gmail.com>
2026-06-04 17:21:21 +0530

    fix bugs

--- backend/Controler/Order.controler.js
+++ backend/Controler/Order.controler.js
@@ -1,4 +1,5 @@
 function makeOrder(req,res){
+    console.log(req.body)
     return res.status(200).json({
         message:"All is working Correctly"
     })
--- backend/Routes/Order.routes.js
+++ backend/Routes/Order.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { makeOrder } from "../Controler/Order.controler.js";
+import { authMiddleware } from "../middleware/Auth.middleware.js";
 const OrderRouter = Router();
 
-OrderRouter.post("/data",makeOrder)
+OrderRouter.post("/data",authMiddleware,makeOrder)
 
 export default OrderRouter;
