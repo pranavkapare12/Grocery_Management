d27f3141d66d7a4210f55dc92b6aed400155033a
Pranav Kapare <sarangkapare12345@gmail.com>
2026-06-04 21:58:12 +0530

    define the schema

---
+++ backend/Database/OrderSchema.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const Schema = new mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    number:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    payment:{
+        type:String,
+        require:true
+    },
+    address:{
+        type:String,
+        require:true
+    },
+    city:{
+        type:String,
+        require:true
+    },
+    state:{
+        type:String,
+        require:true
+    },
+    country:{
+        type:String,
+        require:true
+    },
+    pincode:{
+        type:String,
+        require:true
+    },
+    product:{
+        type:Array,
+        require:true
+    },
+    total:{
+        type:Number,
+        require:true
+    }
+},{timestamps:true})
+
+const orderSchema = mongoose.model("Order",Schema,"OrderDetails");
+
+export default orderSchema;
